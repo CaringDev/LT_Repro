fba20b720b138ca9dd1ff27f1812cf8f5165d221
Raphael Schweizer <raphael.schweizer@bbv.ch>
2023-08-09 18:59:30 +0200

    xUnit / LambdaTale repro

---
+++ UnitTest.cs
@@ -0,0 +1,23 @@
+using Xunit;
+
+namespace TestProject1
+{
+    using LambdaTale;
+
+    public class UnitTest
+    {
+        [Fact]
+        public void Works()
+        {
+        }
+    }
+
+
+    public class ShouldWork
+    {
+        [Scenario]
+        public void ButFails()
+        {
+        }
+    }
+}
